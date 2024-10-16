7ad0fb77f94ef48a4ab57971b950daa5ea0883b0
Brian Sekelsky <bsekels@ncsu.edu>
2024-10-16 07:18:50 -0400

    new map style + a few points

--- src/components/Glaciers.jsx
+++ src/components/Glaciers.jsx
@@ -2,7 +2,14 @@ import { useEffect, useState } from "react";
 import Glacier from "./Glacier.jsx";
 import Renderer from "./Renderer.jsx";
 
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+const map = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.{ext}', {
+    minZoom: 0,
+    maxZoom: 20,
+    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    ext: 'png'
+});
 
 function Glaciers() {
     const [data, setData] = useState(null);
@@ -12,7 +19,7 @@ function Glaciers() {
     const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
-        fetch('/glaciers.json')
+        fetch('/smallTest.json')
             .then(response => response.json())
             .then(data => {
                 setData(data);
@@ -28,10 +35,10 @@ function Glaciers() {
             .catch(error => console.error('Error fetching data:', error));
     }, []);
 
-    // Filter the glacierList based on the search query
-    const filteredGlacierList = glacierList.filter(glacier =>
-        glacier.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
+    // // Filter the glacierList based on the search query
+    // const filteredGlacierList = glacierList.filter(glacier =>
+    //     glacier.name.toLowerCase().includes(searchQuery.toLowerCase())
+    // );
 
     const handleKeyDown = (e) => {
         if (e.key === 'Enter') {
@@ -47,19 +54,26 @@ function Glaciers() {
         <div>
             <div className='sketch border'>
                 {/* <Sketch glaciers={glacierList} /> */}
-                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                {/* <LeafletContext.Provider>
+                    {LeafletContext.tileLayer.provider('Stadia.StamenWatercolor').addTo(map)}
+                </LeafletContext.Provider> */}
+                <MapContainer center={[51.505, -0.09]} zoom={3}>
                     <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png"
+                        provider='Stadia.StamenWatercolor'
                     />
-                    {/* <Marker position={[51.505, -0.09]}>
-                        <Popup>
-                            This is a popup
-                        </Popup>
-                    </Marker> */}
+                    {glacierList.map(glacier => (
+                        console.log(glacier),
+                        <Marker position={[glacier.latitude, glacier.longitude]}>
+                            <Popup>
+                                This is a popup
+                            </Popup>
+                        </Marker>
+                    ))}
                 </MapContainer>
             </div>
-            <div className="w-100 justify-between flex mt-8 mb-4 items-center">
+            {/* <div className="w-100 justify-between flex mt-8 mb-4 items-center">
                 <div className="flex justify-left gap-2">
                     <input
                         type="text"
@@ -80,7 +94,7 @@ function Glaciers() {
                     <button onClick={() => setActiveTab("library")} className={activeTab === "library" ? "active" : ""}>CARDS</button>
                 </div>
             </div>
-            <Renderer myActiveTab={activeTab} myGlaciers={filteredGlacierList} />
+            <Renderer myActiveTab={activeTab} myGlaciers={filteredGlacierList} /> */}
         </div>
     );
 }
